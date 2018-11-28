f57b3a77c21056ce9814c2a6fc1520ffccd57852
cfx <308949219@qq.com>
2018-11-28 15:55:11 +0800

    Initial commit

---
+++ src/com/ppx/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ppx;
+
+/**
+ * Created by admin on 2018/11/26.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
